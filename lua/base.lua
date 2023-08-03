f231e4ff5899d6746703d3a5c3b995b3c9e1638b
yanknvim <yanknvim@gmail.com>
2023-08-03 17:57:36 +0900

    yank: made template

---
+++ lua/base.lua
@@ -0,0 +1,21 @@
+vim.cmd.colorscheme("dracula")
+
+opt = vim.opt
+
+opt.writebackup = false
+opt.backup = false
+opt.swapfile = false
+
+opt.cursorline = true
+opt.number = true
+opt.showmatch = true
+
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.tabstop = 2
+opt.smartindent = true
+
+opt.laststatus = 3
+
+opt.mouse = 'a'
---
+++ lua/plugins.lua
@@ -0,0 +1,6 @@
+require("lazy").setup({
+  require("plugins/colorscheme"),
+  require("plugins/lsp"),
+  require("plugins/completion"),
+  require("plugins/misc"),
+})
